c56a53ddf3ef2839a67921609f3727a78a004718
Marina Galinova <m.kriklivenko@gmail.com>
2022-07-29 00:26:55 +0300

    Вывод трех результатов

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,25 @@ public class Main {
         int loan = 1_000_000; //тело займа (долг)
         double percent = 0.0999; //годовая процентная ставка
         double persentPerMonth = percent / 12; //ежемесячная процентная ставка
-        int month = 36; // количество месяцев
+        int month = 12; // количество месяцев
         double annuitet = payment.calculate(loan, persentPerMonth, month); //расчет аннуитетного платежа
-        System.out.println("Аннуитетный платеж при займе на " + month + " месяцев ");
+        System.out.println("Аннуитетный платеж при займе на " + month + " месяцев: ");
         System.out.printf("%.0f", annuitet);
+        System.out.println("");
+        System.out.println("");
+
+        month = 24; // количество месяцев
+        annuitet = payment.calculate(loan, persentPerMonth, month); //расчет аннуитетного платежа
+        System.out.println("Аннуитетный платеж при займе на " + month + " месяцев: ");
+        System.out.printf("%.0f", annuitet);
+        System.out.println("");
+        System.out.println("");
+
+        month = 36; // количество месяцев
+        annuitet = payment.calculate(loan, persentPerMonth, month); //расчет аннуитетного платежа
+        System.out.println("Аннуитетный платеж при займе на " + month + " месяцев: ");
+        System.out.printf("%.0f", annuitet);
+        System.out.println("");
     }
 
 
